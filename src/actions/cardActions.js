115f7d5d7dec8cc61661cd4607724bca4255eb4f
江冠德 <mr.sunboss@gmail.com>
2016-05-29 18:57:19 +0800

    修改 card 資料流，增加 單卡 fatch action

--- src/actions/cardActions.js
+++ src/actions/cardActions.js
@@ -1,14 +1,13 @@
 import { createAction } from 'redux-actions';
 export const changeTab = createAction('change tab');
-export const fetchCardInfo = createAction('fetch CardInfo');
-export const requestCardInfo = () => {
-  return (dispatch,state) => {
-    const {search} = state();
-    fetch(`http://api.xpg.cards/basicSearch?query=${search.query}&page=${search.current_page}`)
+export const fetchCardInfo = createAction('fetch cardinfo');
+export const requestCardInfo = (serialNumber) => {
+  return (dispatch) => {
+    fetch(`http://api.xpg.cards/card?serialNumber=${serialNumber}`)
       .then((response)=> {
         return response.json()
     }).then((json)=> {
-      dispatch(fetchBasic(json));
+      dispatch(fetchCardInfo(json));
     });
   };
 }
