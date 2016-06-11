c6f9acd93105653902fb50bf0fb2eb14f764df86
江冠德 <mr.sunboss@gmail.com>
2016-06-11 16:49:29 +0800

    新增Ban Reducer 註解

--- src/reducers/banReducer.js
+++ src/reducers/banReducer.js
@@ -18,30 +18,21 @@ const initialState ={
     list:[]
   },
   sumbitBtn: ButtonStateEnum.None,
-  list:[{
-    id: 1,
-    name: "Test1",
-    date: "2016.01.01",
-    enable: true,
-    ban: 20,
-    limit: 10,
-    prelimit: 4
-  },{
-    id: 2,
-    name: "Test2",
-    date: "2016.02.02",
-    enable: false,
-    ban: 30,
-    limit: 20,
-    prelimit: 8
-  }]
+  list:[]
 
 };
 export default handleActions({
+  // 重置表單
+  'fetch init' (state, action) {
+    state.banform = initialState.banform;
+    return Object.assign({},state);
+  },
+  // 讀取搜尋卡片列表
   'fetch cardlist' (state, action) {
     state.searchForm.list = action.payload;
     return Object.assign({},state);
   },
+  // 設定禁卡類型
   'set bantype' (state, action) {
     state.banform.type = action.payload;
     return Object.assign({},state);
@@ -50,16 +41,34 @@ export default handleActions({
     state.sumbitBtn = action.payload;
     return Object.assign({},state);
   },
+  // 更改禁卡表日期
   'change bandate' (state, action) {
     state.banform.date = action.payload;
     return Object.assign({},state);
   },
+  // 更改禁卡表名稱
+  'change name' (state, action) {
+    state.banform.name = action.payload;
+    return Object.assign({},state);
+  },
+  // 加入至禁卡表表單
   'add tolist' (state, action) {
     action.payload.type = state.banform.type;
     let IsExist = state.banform.list.filter(data=>data.serial_number === action.payload.serial_number)
     if(IsExist.length ===0){
       state.banform.list.push(action.payload);
     }
+    return Object.assign({},state);
+  },
+  // 讀取禁卡表列表
+  'fetch banlist' (state, action) {
+    state.list = action.payload;
+    return Object.assign({},state);
+  },
+  // 讀取禁卡表單
+  'fetch banform' (state, action) {
+    state.banform = action.payload;
+    state.banform.date = moment(state.banform.date);
     return Object.assign({},state);
   }
 }, initialState);
