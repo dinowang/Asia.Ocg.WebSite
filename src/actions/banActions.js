27f5377c466d9fdcb7708584512b28351fcdfefc
江冠德 <mr.sunboss@gmail.com>
2016-06-11 16:49:09 +0800

    新增Get BanList API

--- src/actions/banActions.js
+++ src/actions/banActions.js
@@ -1,9 +1,13 @@
 import { createAction } from 'redux-actions';
 export const fetchCardList = createAction('fetch cardlist');
+export const fetchBanList = createAction('fetch banlist');
+export const fetchBanForm = createAction('fetch banform');
+export const fetchInit = createAction('fetch init');
 export const setBanType = createAction('set bantype');
 export const addToList = createAction('add tolist');
 export const changeBtnType = createAction('change btntype');
 export const changeBanDate = createAction('change bandate');
+export const changeName = createAction('change name');
 export const requestSearch = (value) => {
   return (dispatch) => {
     fetch(`http://10.211.55.3/Asia.Ocg.WebAPI/search/${value}`)
@@ -20,7 +24,7 @@ export const requestCreateBan = () => {
   return (dispatch, state) => {
     const {ban} = state();
     console.log('ban',ban.banform);
-    fetch(`http://10.211.55.3/Asia.Ocg.WebAPI/ban`,{
+    fetch(`http://10.211.55.3/Asia.Ocg.WebAPI/manage/ban`,{
       method:'POST',
       headers: {
         'Accept': 'application/json',
@@ -34,5 +38,30 @@ export const requestCreateBan = () => {
       console.log(json);
     });
 
+  };
+};
+// Manage
+export const requestManageBanList = (value) => {
+  return (dispatch) => {
+    fetch(`http://10.211.55.3/Asia.Ocg.WebAPI/manage/ban`)
+      .then((response)=> {
+        return response.json();
+    }).then((json)=> {
+      if(json.data){
+        dispatch(fetchBanList(json.data));
+      }
+    });
+  };
+};
+export const requestManageBanForm = (id) => {
+  return (dispatch) => {
+    fetch(`http://10.211.55.3/Asia.Ocg.WebAPI/manage/ban/${id}`)
+      .then((response)=> {
+        return response.json();
+    }).then((json)=> {
+      if(json.data){
+        dispatch(fetchBanForm(json.data));
+      }
+    });
   };
 };
