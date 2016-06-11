0a5652fa0a18ac9397b145e7ac332a180c5528fa
江冠德 <mr.sunboss@gmail.com>
2016-06-11 16:50:40 +0800

    新增加入禁卡表按鈕、清除表單 Action

--- src/containers/Manage/Ban/BanFormPage/index.js
+++ src/containers/Manage/Ban/BanFormPage/index.js
@@ -14,57 +14,78 @@ import BanTypeEnum from '../../../../enums/banTypeEnum';
 
 import './index.scss';
 
-export const BanFormPage = (props) => {
-  const createBtn =()=>{
+class BanFormPage extends React.Component {
+  constructor() {
+    super();
+    this.onDateChange = this.onDateChange.bind(this);
+    this.dropValue = this.dropValue.bind(this);
+    this.panelOnClick = this.panelOnClick.bind(this);
+    this.save = this.save.bind(this);
+    this.searchOnChange = this.searchOnChange.bind(this);
+    this.nameOnChange = this.nameOnChange.bind(this);
+
+  }
+  componentWillMount(){
+    let {id} = this.props.params;
+    if(id){
+      this.props.actions.requestManageBanForm(id);
+    }else{
+      this.props.actions.fetchInit();
+    }
+  }
+  onDateChange(e){
+    this.props.actions.changeBanDate(e);
   };
-  const onDateChange = (e)=>{
-    props.actions.changeBanDate(e);
+  dropValue(e){
+    this.props.actions.setBanType(e.key);
   };
-  const dropValue = (e)=>{
-    props.actions.setBanType(e.key);
-  };
-  const panelOnClick = (cardData)=>{
-    if(props.ban.banform.type >=0){
-      props.actions.addToList(cardData);
+  panelOnClick(cardData){
+    if(this.props.ban.banform.type >=0){
+      this.props.actions.addToList(cardData);
     }
   };
-  const save = ()=>{
-    props.actions.changeBtnType(ButtonStateEnum.Loading)
+  save(){
+    // this.props.actions.changeBtnType(ButtonStateEnum.Loading)
 
-    props.actions.requestCreateBan();
+    this.props.actions.requestCreateBan();
   };
-  let onChangeEvent;
-  const searchOnChange = (value)=>{
+  searchOnChange(value){
     value = value.toUpperCase();
-    clearTimeout(onChangeEvent);
-    onChangeEvent = setTimeout(()=>{
-      props.actions.requestSearch(value);
+    clearTimeout(this.onChangeEvent);
+    this.onChangeEvent = setTimeout(()=>{
+      this.props.actions.requestSearch(value);
     },500);
 
   };
-  const switchStyle = {top:"10px"};
-  return (
-    <div className="bancreate-page">
-      <div className="form">
-        <input type="text" placeholder="禁卡表名稱"/>
-          <DatePicker
-    dateFormat="YYYY/MM/DD"
-    selected={props.ban.banform.date}
-    onChange={onDateChange} />
-  <DropDown style={{top:'1px'}} getValue={dropValue} default={-1} values={[{key:-1,value:'請選擇'},{key:BanTypeEnum.Ban,value:'禁止'},{key:BanTypeEnum.Limit,value:'限制'},{key:BanTypeEnum.PreLimit,value:'準限制'}]}/>
-          <SwitchButton style={switchStyle}/>
-          <Button onClick={save} style={{float:'right',top:'5px'}} state={props.ban.sumbitBtn} rIcon="floppy-o" value="存擋" fail="fail" success="success"/>
+  nameOnChange(e){
+    console.log('nameOnChange',e.target.value)
+    this.props.actions.changeName(e.target.value);
+  }
+  render(){
+    const switchStyle = {top:"10px"};
+    return (
+      <div className="bancreate-page">
+        <div className="form">
+          <input type="text" placeholder="禁卡表名稱" onChange={this.nameOnChange} value={this.props.ban.banform.name}/>
+            <DatePicker
+              dateFormat="YYYY/MM/DD"
+              selected={this.props.ban.banform.date}
+              onChange={this.onDateChange} />
+            <DropDown style={{top:'1px'}} getValue={this.dropValue} default={-1} values={[{key:-1,value:'請選擇'},{key:BanTypeEnum.Ban,value:'禁止'},{key:BanTypeEnum.Limit,value:'限制'},{key:BanTypeEnum.PreLimit,value:'準限制'}]}/>
+            <SwitchButton style={switchStyle} checked={this.props.ban.banform.enable}/>
+            <Button onClick={this.save} style={{float:'right',top:'5px'}} state={this.props.ban.sumbitBtn} rIcon="floppy-o" value="存擋" fail="fail" success="success"/>
 
-        <hr/>
-        <div className="data">
-          <BanListText data={props.ban.banform}/>
+          <hr/>
+          <div className="data">
+            <BanListText data={this.props.ban.banform}/>
+          </div>
+        </div>
+        <div className="search-list">
+          <SearchListPanel itemOnClick={this.panelOnClick} onChange={this.searchOnChange} data={this.props.ban.searchForm.list}/>
         </div>
       </div>
-      <div className="search-list">
-        <SearchListPanel itemOnClick={panelOnClick} onChange={searchOnChange} data={props.ban.searchForm.list}/>
-      </div>
-    </div>
-  );
+    );
+  }
 };
 
 BanFormPage.propTypes = {
--- src/containers/Manage/Ban/BanListPage/index.js
+++ src/containers/Manage/Ban/BanListPage/index.js
@@ -3,20 +3,26 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import DatePicker from 'react-datepicker';
 import {Icon} from "react-fa";
-import 'react-datepicker/dist/react-datepicker.css';
+import LinkButton from '../../../../components/linkButton';
 import * as actions from '../../../../actions/banActions';
 import {Link} from 'react-router';
+import moment from 'moment';
+
 
 import './index.scss';
 
-export const BanListPage = (props) => {
-  const renderData = (data, index) => {
+class BanListPage extends React.Component {
+  componentWillMount(){
+    this.props.actions.requestManageBanList();
+  }
+  renderData(data, index) {
     const href = `/banManage/Form/${data.id}`
     const enableStyle = data.enable ? {backgroundColor:'#00a65a'} :{backgroundColor:'#dd4b39'};
+    const date = moment(data.date).format("YYYY.MM.DD");
     return (
       <tr key={index}>
         <td>{data.name}</td>
-        <td>{data.date}</td>
+        <td>{date}</td>
         <td style={enableStyle}>{data.enable}</td>
         <td>{data.ban}</td>
         <td>{data.limit}</td>
@@ -31,28 +37,31 @@ export const BanListPage = (props) => {
       </tr>
     )
   };
-  return (
-    <div className="banlist-page">
-      <div className="list">
-        <table>
-          <tbody>
-            <tr>
-              <th>顯示名稱</th>
-              <th>禁卡表日期</th>
-              <th>狀態</th>
-              <th>限制</th>
-              <th>禁止</th>
-              <th>準限制</th>
-              <th></th>
-
-            </tr>
-            {props.ban.list.map(renderData)}
-
-          </tbody>
-        </table>
+  render(){
+    return (
+      <div className="banlist-page">
+        <div className="fun-bar">
+          <LinkButton value="新增" to="/banManage/form" lIcon="plus-square-o"/>
+        </div>
+        <div className="list">
+          <table>
+            <tbody>
+              <tr>
+                <th>顯示名稱</th>
+                <th>禁卡表日期</th>
+                <th>狀態</th>
+                <th>限制</th>
+                <th>禁止</th>
+                <th>準限制</th>
+                <th></th>
+              </tr>
+              {this.props.ban.list.map(this.renderData)}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 BanListPage.propTypes = {
