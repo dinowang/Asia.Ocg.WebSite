a8c45053ba7b29af199a7f864f3b702ff4f6f577
江冠德 <mr.sunboss@gmail.com>
2016-06-27 02:14:23 +0800

    刪除測試資料

--- src/reducers/indexReducer.js
+++ src/reducers/indexReducer.js
@@ -2,11 +2,7 @@ import { handleActions } from 'redux-actions';
 const initialState ={
   card_pop: [],
   product_info: [],
-  deck_pop:[{
-    views: 20,
-    name: 'エスポワールゲーム 第4章 ～The Lottery～',
-    guid:'guid'
-  }]
+  deck_pop:[]
 };
 export default handleActions({
   'fetch indexinfo' (state, action) {
