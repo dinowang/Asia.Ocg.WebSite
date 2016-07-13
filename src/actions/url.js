7a0f2bdd19f061b70fd47db1aeda312182bda580
江冠德 <mr.sunboss@gmail.com>
2016-07-13 19:48:48 +0800

    修改 url

--- src/actions/url.js
+++ src/actions/url.js
@@ -1,2 +1,2 @@
-// export const Host = 'https://api.xpg.cards';
-export const Host = 'http://10.211.55.3/Asia.Ocg.WebAPI';
+export const Host = 'https://api.xpg.cards';
+// export const Host = 'http://10.211.55.3/Asia.Ocg.WebAPI';
