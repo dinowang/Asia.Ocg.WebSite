127f2acf9908faa5056232cc8bf715b340f50341
江冠德 <mr.sunboss@gmail.com>
2016-07-14 17:35:03 +0800

    修正第一次瀏覽會閃白情形

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import { ReduxAsyncConnect, asyncConnect, reducer as reduxAsyncConnect } from 'redux-async-connect'
-const store = configureStore();
+
+import createStore from './store/create';
+const store = createStore(window.__data);
 
 const gaPageView =()=> {
   ReactGA.set({ page: window.location.pathname });
